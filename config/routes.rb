2fe0d656edade2fb4a43d606c6496d5a67442b93
yamane yusuke <yusukeyamane01@gmail.com>
2015-10-24 00:45:03 +0900

    gem 'erb2haml' added

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only:  [:new, :index, :new]
+  resources :login, only: [:index]
+  resources :top, only: [:index]
+  resources :proto, only: [:index, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'top#index'
-
-  get  '/top'               =>    'top#index'
-  get  '/proto'             =>    'proto#index'
-  get  '/proto/new'         =>    'proto#new'
-  get  '/user'              =>    'user#index'
-  get  '/user/new'          =>    'user#new'
-  get  '/user/edit'         =>    'user#edit'
-  get  '/login'             =>    'login#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
