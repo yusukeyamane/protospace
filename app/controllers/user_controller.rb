05fbd6c7293b20a5c6ec6a89c49fb4e470f5af69
yamane yusuke <yusukeyamane01@gmail.com>
2015-10-24 21:01:33 +0900

    add edit new index actions to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,13 @@
 class UserController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :description, :avatar)
