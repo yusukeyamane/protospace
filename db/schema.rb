c6a189f7bb8fc745049f5feb5ee87d1205c13e21
yamane yusuke <yusukeyamane01@gmail.com>
2015-10-26 11:17:06 +0900

    generate prototype_images table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025132610) do
+ActiveRecord::Schema.define(version: 20151026021600) do
 
   create_table "proto_type_images", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
