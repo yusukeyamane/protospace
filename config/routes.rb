287b7b950ee6f19f39f18c84d4fac99c0c1b6cc3
yamane yusuke <yusukeyamane01@gmail.com>
2015-10-28 09:20:37 +0900

    ヘッダーのプロトタイプ投稿へのパスを変更 routesの記述を変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:edit, :index, :update]
-  resources :prototypes, only: [:index, :new]
+  resources :prototypes, only: [:show, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
