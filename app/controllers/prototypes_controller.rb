2c4deaa35cf9dc04f1a8462398c49a734a8139a3
yamane yusuke <yusukeyamane01@gmail.com>
2015-10-29 10:23:40 +0900

    createアクションとrootを作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,5 +4,14 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
+  end
+
+  # def create
+  # end
+
+  private
+  def prototype_params
+    params.permit(:title, :avatar, :catch_copy, :concept)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:edit, :index, :update]
-  resources :prototypes, only: [:show, :new, :update]
+  resources :prototypes, only: [:show, :new, :update, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
