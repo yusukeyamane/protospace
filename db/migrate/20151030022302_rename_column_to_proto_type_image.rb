0c54880c73570cbda2df7e3283f606b734cdc435
yamane yusuke <yusukeyamane01@gmail.com>
2015-10-30 16:51:14 +0900

    不要なファイル、フォルダを削除

--- db/migrate/20151030022302_rename_column_to_proto_type_image.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnToProtoTypeImage < ActiveRecord::Migration
-  def change
-    rename_column :proto_type_images_table, :main_image, :images
-  end
-end
--- db/migrate/20151030022346_rename_column_to_proto_type_images.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumnToProtoTypeImages < ActiveRecord::Migration
-  def change
-    rename_column :proto_type_images, :main_image, :images
-    rename_column :proto_type_images, :sub_image, :sub_eimage
-  end
-end
