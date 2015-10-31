2f1808de4c6f1377e20058125d942d028e3a5c37
yamane yusuke <yusukeyamane01@gmail.com>
2015-11-01 00:24:38 +0900

    prototype_imagesの保存周りのエラーを解決

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,6 +1,6 @@
 class PrototypeImage < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
-  enum status: { main: 1, sub: 2 }
+  enum status: { main: "1", sub: "2" }
   belongs_to :prototype
 end
