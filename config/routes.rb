ba10597e5ac524df4f545b6aa6d36f04dcbdc564
yamane yusuke <yusukeyamane01@gmail.com>
2015-11-03 09:48:23 +0900

    resources の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:edit, :show, :update]
-  resources :prototypes, only: [:show, :new, :update, :create]
+  resources :prototypes, only: [:show, :new, :update, :create, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
