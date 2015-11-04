e98ba7dd18ea9e2724d5675b344cdc955799a93a
yamane yusuke <yusukeyamane01@gmail.com>
2015-11-04 13:52:28 +0900

    prototypeとprototypeモデルにvalidationを設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,5 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images
 
+  validates :title,:catch_copy,:concept,  presence: true
   accepts_nested_attributes_for :prototype_images
+
 end
