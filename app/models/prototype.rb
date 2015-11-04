bd6ca514ada058b5d2a1d6b5f4f8035e134afd52
yamane yusuke <yusukeyamane01@gmail.com>
2015-11-04 16:55:35 +0900

    validationの設定を改善

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :prototype_images
+  has_many :prototype_images, inverse_of: :prototype
 
   validates :title, :catch_copy, :concept, presence: true
   accepts_nested_attributes_for :prototype_images
--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -2,6 +2,6 @@ class PrototypeImage < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   enum status: { main: "1", sub: "2" }
-  validates :image, presence: true
   belongs_to :prototype
+  validates_presence_of :image
 end
