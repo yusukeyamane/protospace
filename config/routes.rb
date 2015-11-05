f022d6255936728dd48e672f83bd7dfb9bf65fec
yamane yusuke <yusukeyamane01@gmail.com>
2015-11-05 15:26:00 +0900

    resourcesでコメントをprototypeにネスト化

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:edit, :index, :update]
-  resources :prototypes, only: [:show, :new, :update, :create]
+  resources :prototypes, only: [:show, :new, :update, :create] do
+    resources :comments, only:[:new,:create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
