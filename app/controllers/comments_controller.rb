1680c43b5a401b2706162c06f2a03ff2c0b1deee
yamane yusuke <yusukeyamane01@gmail.com>
2015-11-08 19:47:27 +0900

    commentsコントローラーをajax用に変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comments_params)
+    @comments = Comment.where(prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
     current_user.comments.create(comments_params)
+    render layout: false
   end
 
   private
   def comments_params
-    params.require(:comment).permit(:content).merge(prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:text).merge(prototype_id: params[:prototype_id])
   end
 end
