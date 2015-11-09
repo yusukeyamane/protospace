a7de888e8dfdc6690a4ffa08d237ac768a574762
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-09 19:37:30 +0900

    db:migrate done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108043714) do
+ActiveRecord::Schema.define(version: 20151109102705) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",         limit: 65535
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20151108043714) do
     t.integer  "prototype_id", limit: 4
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.string   "tag_type",     limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "prototype_images", force: :cascade do |t|
