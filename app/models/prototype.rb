4d835fdef2bd5b6120276fd4cf028195a2b6fc17
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-11 01:40:37 +0900

    fix typo

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,7 +4,7 @@ class Prototype < ActiveRecord::Base
   has_many :comments
   has_many :prototype_images, inverse_of: :prototype
 
-  def like_usr(user)
+  def like_user(user)
     likes.find_by(user_id: user)
   end
   validates :title, :catch_copy, :concept, presence: true
