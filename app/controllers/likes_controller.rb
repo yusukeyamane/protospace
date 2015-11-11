b130034d78191405f59456e6e14c57f5b36f1124
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-11 22:08:36 +0900

    likeのcreateアクションを作成。※destroyは未完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @prototype = Prototype.find(params[:prototype_id])
+    @like = current_user.likes.create(create_params)
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.delete if @like.user_id == current_user.id
+  end
+
+  private
+  def create_params
+    params.permit(:prototype_id)
+  end
 end
