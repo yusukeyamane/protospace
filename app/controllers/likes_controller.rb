a54d94ae301c385242b3d78b448c9ea8f1b354bb
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-13 03:13:17 +0900

    likes controllerのcreateアクションとdeleteアクションを修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,14 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!
+
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @like = current_user.likes.create(create_params)
   end
 
   def destroy
+    @prototype = Prototype.find(params[:prototype_id])
     @like = Like.find(params[:id])
-    @like.delete if @like.user_id == current_user.id
+    @like.destroy
   end
 
   private
