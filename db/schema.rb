e7355f0fd0f11eee7e54d196e7c205efd24d1d3f
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-13 18:29:41 +0900

    acts_as_taggable_on_engine:install:migrationsを実行。出来たマイグレーションファイルを実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111095241) do
+ActiveRecord::Schema.define(version: 20151113092421) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",         limit: 65535
@@ -46,12 +46,17 @@ ActiveRecord::Schema.define(version: 20151111095241) do
     t.integer  "tag_id",     limit: 4
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string   "title",        limit: 255
-    t.integer  "prototype_id", limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id",        limit: 4
+    t.integer  "taggable_id",   limit: 4
+    t.string   "taggable_type", limit: 255
+    t.integer  "tagger_id",     limit: 4
+    t.string   "tagger_type",   limit: 255
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
   end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
