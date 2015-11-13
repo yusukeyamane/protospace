9cc96ef6b8559ee268fce77384f1c6ccdf8db1e9
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-13 18:54:59 +0900

    tagのテーブルを作成しなおした

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113092421) do
+ActiveRecord::Schema.define(version: 20151113095044) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",         limit: 65535
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 20151113092421) do
   end
 
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count", limit: 4,   default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
