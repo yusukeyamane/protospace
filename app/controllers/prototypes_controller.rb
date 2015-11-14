cbb11a70a2889de36844505173b8d8e672b46344
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-14 23:09:32 +0900

    newestページでprototypeが投稿順に並ぶようにした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,6 +29,10 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
+  def newest
+    @prototypes = Prototype.includes(:prototype_images).order('created_at DESC').page(params[:page]).per(8)
+  end
+
   private
   def prototype_find
     @prototype = Prototype.find(params[:id])
