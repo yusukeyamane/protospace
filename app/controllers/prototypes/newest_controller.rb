954da663aa32c8f5211ea786db8674d980417fe6
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-17 00:14:09 +0900

    paginates_perをモデルに記述することでモデルのコード数を減らした

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,5 +1,5 @@
 class Prototypes::NewestController < ApplicationController
   def index
-    @prototypes = Prototype.includes(:prototype_images).order('created_at DESC').page(params[:page]).per(8)
+    @prototypes = Prototype.includes(:prototype_images).order('created_at DESC')
   end
 end
