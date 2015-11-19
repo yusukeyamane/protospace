b96ba453203090babd21a88f2462886bd44a7a3c
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-19 18:22:18 +0900

    プレビューを表示させるためのjsファイルを追加

---
+++ app/assets/javascripts/preview_image.js
@@ -0,0 +1,20 @@
+$(window).on('page:update page:load', function() {
+
+  $('.main_image_preview input:file').uploadThumbs({
+  });
+  $('.sub_image_preview1 input:file').uploadThumbs({
+  });
+  $('.sub_image_preview2 input:file').uploadThumbs({
+  });
+  $('#edit-main-image input:file').uploadThumbs({
+  });
+  $('#edit-sub-image1 input:file').uploadThumbs({
+  });
+  $('#edit-sub-image2 input:file').uploadThumbs({
+  });
+  $('#new-user-avatar input:file').uploadThumbs({
+  });
+  $('#edit-user-avatar input:file').uploadThumbs({
+  });
+
+});
