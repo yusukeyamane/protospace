f777be3501bb2943a75a07d562d75aac279aecb8
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-20 01:29:12 +0900

    statusを数字で管理できるようにviewとモデルを変更

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,7 +1,7 @@
 class PrototypeImage < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
-  enum status: { main: "main", sub: "sub" }
+  enum status: { main: "1", sub: "2" }
   belongs_to :prototype
   validates_presence_of :image
 end
