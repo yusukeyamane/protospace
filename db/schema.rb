d05946c170d000f947a062aefc4202cef815a12b
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-20 18:24:18 +0900

    カラムの方を変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119162230) do
+ActiveRecord::Schema.define(version: 20151120091356) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",         limit: 65535
