0f6a8e2cb39b6c122e29ef6c646e75b1023897f3
yusukeyamane <yusukeyamane01@gmail.com>
2015-11-20 18:23:27 +0900

    カラムの方をintegerに変更

--- db/migrate/20151120091356_change_status_to_prototype_images.rb
+++ db/migrate/20151120091356_change_status_to_prototype_images.rb
@@ -1,5 +1,5 @@
 class ChangeStatusToPrototypeImages < ActiveRecord::Migration
   def change
-    change_column :prototype_images, :status, :integer, null: false, default: 0
+    change_column :prototype_images, :status, :integer, null: false
   end
 end
